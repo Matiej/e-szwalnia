ff89195435b0eb722727799f3ed3aa4942d02738
Maciej Wojcik <maciek@testaarosa.pl>
2019-07-07 19:06:30 +0200

    FIRST COMMIT

---
+++ src/main/java/com/eszwalnia/timesh/TimeshApplication.java
@@ -0,0 +1,13 @@
+package com.eszwalnia.timesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeshApplication.class, args);
+	}
+
+}
