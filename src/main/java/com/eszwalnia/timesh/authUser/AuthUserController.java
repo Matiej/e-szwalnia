cfeb04bfa9010c148d7bd12551f35cf788d7174d
Maciej Wojcik <maciek@testaarosa.pl>
2019-08-17 22:17:58 +0200

    update authUser feature fo controller and services
    add method to repository to check emilas

--- src/main/java/com/eszwalnia/timesh/authUser/AuthUserController.java
+++ src/main/java/com/eszwalnia/timesh/authUser/AuthUserController.java
@@ -28,7 +28,6 @@ public class AuthUserController {
     @ApiOperation(value = "Create new authorization user", response = AuthUserDto.class)
     @ApiResponses(value = {
             @ApiResponse(code = 201, message = "Auth user created and saved to data base successful"),
-            @ApiResponse(code = 404, message = "Server has not found anything matching the requested URI! Auth user not created!"),
             @ApiResponse(code = 409, message = "Auth user already exists, can't create"),
             @ApiResponse(code = 503, message = "Data base server error. Can't create auth user.")})
     ResponseEntity<Object> create(@RequestBody @Valid CreateAuthUserDto createAuthUserDto) {
@@ -42,13 +41,27 @@ public class AuthUserController {
                 .body(createdAuthUser);
     }
 
+    //todo do poprawy prawdopodobnie-> z forntu będzie brany emial lub login i dopiero z formatki
+    //todo updejt
+    @PutMapping("/update")
+    @ApiOperation(value = "Update auth user in data base", response = AuthUserDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Auth user updated successful"),
+            @ApiResponse(code = 401, message = "No remote user found or not user logged in"),
+            @ApiResponse(code = 404, message = "Auth user does not exist! Can't update!"),
+            @ApiResponse(code = 409, message = "Email/login already exists, can't update"),
+            @ApiResponse(code = 503, message = "Data base server error. Can't update auth user.")})
+    ResponseEntity<Object> update(@RequestBody @Valid AuthUserDto authUserDto) {
+        AuthUserDto updatedAuthUser = authUserService.updateAuthUser(authUserDto);
+        return ResponseEntity.ok(updatedAuthUser);
+    }
+
     @GetMapping("/findall")
     @ApiOperation(value = "Find all authUsers in data base", response = AuthUserDto.class)
     @ApiResponses(value = {
-            @ApiResponse(code = 503, message = "Server error. Can't get any users."),
             @ApiResponse(code = 200, message = "All users loaded from db successful."),
-//            @ApiResponse(code = 400, message = "No measuring stations found!"),
-            @ApiResponse(code = 404, message = "Server has not found anything matching the requested URI! No users found!")})
+            @ApiResponse(code = 404, message = "Server has not found anything matching the requested URI! No users found!"),
+            @ApiResponse(code = 503, message = "Server error. Can't get any users."),})
     ResponseEntity<Object> findAll() {
         return ResponseEntity.ok(authUserService.findAll());
     }
--- src/main/java/com/eszwalnia/timesh/authUser/domain/AuthUserService.java
+++ src/main/java/com/eszwalnia/timesh/authUser/domain/AuthUserService.java
@@ -1,7 +1,9 @@
 package com.eszwalnia.timesh.authUser.domain;
 
+import com.eszwalnia.timesh.authUser.AuthUserNotFoundException;
 import com.eszwalnia.timesh.authUser.domain.dto.AuthUserDto;
 import com.eszwalnia.timesh.authUser.domain.dto.CreateAuthUserDto;
+import com.eszwalnia.timesh.exceptionHandler.ExistEmailException;
 
 import java.util.List;
 
@@ -10,4 +12,8 @@ public interface AuthUserService {
     AuthUserDto createAuthUser(CreateAuthUserDto CreateAuthUserDto);
 
     List<AuthUserDto> findAll();
+
+    AuthUserDto updateAuthUser(AuthUserDto authUserDto) throws ExistEmailException, AuthUserNotFoundException;
+
+    void deleteAuthUser(Long id);
 }
