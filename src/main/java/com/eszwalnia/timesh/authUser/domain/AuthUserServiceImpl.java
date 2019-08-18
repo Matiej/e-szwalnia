a261a8640aecdab243e9d9175eadceb610e7a17e
Maciej Wojcik <maciek@testaarosa.pl>
2019-08-18 11:35:15 +0200

    add log for authUser implementation
    add mvn depedency for slf4j and add
    basic configuration in yaml file

--- src/main/java/com/eszwalnia/timesh/authUser/domain/AuthUserServiceImpl.java
+++ src/main/java/com/eszwalnia/timesh/authUser/domain/AuthUserServiceImpl.java
@@ -8,6 +8,7 @@ import com.eszwalnia.timesh.exceptionHandler.ExistEmailException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.HibernateException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +20,7 @@ import java.util.stream.Collectors;
 
 @Getter
 @Setter
+@Slf4j
 @RequiredArgsConstructor
 class AuthUserServiceImpl implements AuthUserService {
 
@@ -37,6 +39,8 @@ class AuthUserServiceImpl implements AuthUserService {
                         " Can't CREATE user!");
             }
             authUserRepository.save(createdAuthUser);
+            log.info("Created auth user: " + "\nID-> " + createdAuthUser.getId() + "\nEmail->" + createdAuthUser.getEmail());
+            log.debug("Created auth user: {} \n {} \n {}", createdAuthUser,createdAuthUser.getAuthUserRoleSet(), createdAuthUser.getEmployee());
             return authUserMapper.authUserToDto(createdAuthUser, new CycleAvoidingMappingContext());
         } catch (HibernateException e) {
             throw new RuntimeException("Can't find auth users because of some db error ", e);
@@ -46,9 +50,11 @@ class AuthUserServiceImpl implements AuthUserService {
     @Override
     public List<AuthUserDto> findAll() throws HibernateException {
         try {
-            return authUserRepository.findAll().stream()
+            List<AuthUserDto> authUserDtoList = authUserRepository.findAll().stream()
                     .map(a -> authUserMapper.authUserToDto(a, new CycleAvoidingMappingContext()))
                     .collect(Collectors.toList());
+            log.info("Found -> " + authUserDtoList.size() + " auth users in data base");
+            return authUserDtoList;
         } catch (HibernateException e) {
             throw new RuntimeException("Can't find auth users because of some db error ", e);
         }
@@ -57,21 +63,25 @@ class AuthUserServiceImpl implements AuthUserService {
     @Override
     public AuthUserDto updateAuthUser(AuthUserDto authUserDto) throws ExistEmailException, HibernateException,
             AuthUserNotFoundException {
+        log.debug("Update data for auth user: {} \n {} \n {}", authUserDto,authUserDto.getAuthUserRoleSet(), authUserDto.getEmployee());
         try {
             Optional<AuthUser> authUser = authUserRepository.findById(authUserDto.getId());
             if (authUser.isPresent()) {
-                if (isUserEmailExist(authUser.get().getId(), authUserDto.getEmail())) {
+                AuthUser foundAuthUser = authUser.get();
+                log.debug("Old data for auth user: {} \n {} \n {}", foundAuthUser, foundAuthUser.getAuthUserRoleSet(), foundAuthUser.getEmployee());
+                if (isUserEmailExist(foundAuthUser.getId(), authUserDto.getEmail())) {
                     throw new ExistEmailException("There is an account with that email address: " + authUserDto.getEmail() +
-                            " Can't update user!");
+                            ". Can't update user!");
                 }
                 AuthUser authUserToSave = authUserMapper.authUserDtoToEntity(authUserDto, new CycleAvoidingMappingContext());
                 AuthUser savedAuthUser = authUserRepository.save(authUserToSave);
+                log.info("Auth user: " + "\nID-> " + savedAuthUser.getId() + "\nEmail->" + savedAuthUser.getEmail() + " UPDATED!");
                 return authUserMapper.authUserToDto(savedAuthUser, new CycleAvoidingMappingContext());
             } else {
                 throw new AuthUserNotFoundException("Auth user: " + authUserDto.getEmail() + " does not exist! Can't update!");
             }
         } catch (HibernateException e) {
-            throw new RuntimeException("Can't  update auth user because of some db error ", e);
+            throw new RuntimeException("Can't update auth user because of some db error ", e);
         }
     }
 
@@ -80,8 +90,10 @@ class AuthUserServiceImpl implements AuthUserService {
         boolean isDeleted;
         try {
             if(authUserRepository.existsById(id)) {
+                log.warn("Going to delete auth user id: " + id);
                 authUserRepository.deleteById(id);
                 isDeleted = true;
+                log.info("Auth user id: " + id + " deleted!"); ;
             } else {
                 throw new AuthUserNotFoundException("Auth user id: " + id + " isn't exist. Cant' delete");
             }
